72ddf09535a5da40b820786332d64c6130954af6
Tanaya Motic Permatasari <43507919+TanayaMoticP@users.noreply.github.com>
2018-10-05 10:13:01 +0700

    Update regristrasi.php
    nama file formReg.

--- regristrasi.php
+++ regristrasi.php
@@ -3,7 +3,36 @@
 <head>
 	<title>Form Registrasi</title>
 </head>
-<body>
+<body
+<form action="" method="POST">
+	<table>
+	<tr>
+		<td>NIM</td>
+		<td>:</td>
+		<td><input type ="text" name="NIM" length="10"></td>
+
+		<td>nama</td>
+		<td>:</td>
+		<td><input type="text" name="nama" length="25"></td>
+
+		<td>email</td>
+		<td>:</td>
+		<td><input type="text" name="email"></td>
+
+
+		<td></td>
+		<td><input type="submit" name="kirim" value="kirim"></td>
+
+	</tr>
+	</table>
+</form>
+
+
+<?php 
+	if(isset($_POST['kirim'])){
+		include "formReg"
+	}
+ ?>
 
 
 
