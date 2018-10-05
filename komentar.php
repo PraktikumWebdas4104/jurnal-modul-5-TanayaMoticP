33751cf3c2d8393b9a56c8db706bd252700597df
Tanaya Motic Permatasari <43507919+TanayaMoticP@users.noreply.github.com>
2018-10-05 10:21:46 +0700

    Update komentar.php
    komentar untuk contoh NIM

--- komentar.php
+++ komentar.php
@@ -1,3 +1,25 @@
-<?php
+<form action="" method="POST">
+	<table>
+	<tr>
+		<td>NIM</td>
+		<td>:</td>
+		<td><textarea name="komentar"></textarea></td>
+		
 
-?>
+		<td></td>
+		<td><input type="submit" name="kirim" value="kirim"></td>
+
+	</tr>
+	</table>
+</form>
+ <?php 
+ if (isset($_POST['kirim'])) {
+  	$komentar = $_POST['komentar'];
+
+  	if (str_word_count($komentar)<5) {
+  		echo "Maukkan Angka Dengan maksimal 10 digit";
+  	}else{
+  		echo str_word_count($komentar);
+
+  	}
+  } ?>
